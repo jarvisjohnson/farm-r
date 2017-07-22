9b37373aa04703822dd90fd28263aa3490566574
Jarvis Johnson <jarviscjohnson@gmail.com>
2017-07-22 10:15:54 -0400

    remove foreign keys

--- db/migrate/20170715214851_add_currency_to_stripe_payments.rb
+++ db/migrate/20170715214851_add_currency_to_stripe_payments.rb
@@ -1,9 +1,9 @@
 class AddCurrencyToStripePayments < ActiveRecord::Migration[5.1]
   def change
-    change_table :stripe_payments do |t|
-      t.integer  "sum_cents"
-      t.string   "currency"
-      t.string   "stripe_transaction_id"
-    end
+    # change_table :stripe_payments do |t|
+    #   t.integer  "sum_cents"
+    #   t.string   "currency"
+    #   t.string   "stripe_transaction_id"
+    # end
   end
 end
